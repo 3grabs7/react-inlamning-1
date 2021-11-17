8eb574d82c8bcfcd701d88e9a77071e768f4e9ec
Robin Grabs <robinmgrabs@gmail.com>
2021-11-18 00:29:27 +0100

    feature: add spinning class to logo when mouse over nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,55 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.scss';
 import { useLocation, useNavigate } from 'react-router-dom';
 import RoutePaths from '../routes/RoutePaths';
+import logo from '../shared/logo.png';
 
 export const Nav = () => {
 	const navigate = useNavigate();
 	const currentView = useLocation();
 
+	const [navHover, setNavHover] = useState(false);
+
 	return (
-		<div>
+		<div className='navbar-top'>
 			<ul>
-				<li
-					className={
-						currentView.pathname === RoutePaths.home ? 'active-view' : ''
-					}
-					onClick={() => {
-						navigate(RoutePaths.home);
+				<div
+					className='left'
+					onMouseOver={() => {
+						setNavHover(true);
+					}}
+					onMouseLeave={() => {
+						setNavHover(false);
 					}}
 				>
-					Home
-				</li>
-				<li
-					className={
-						currentView.pathname === RoutePaths.smash ? 'active-view' : ''
-					}
-					onClick={() => {
-						navigate(RoutePaths.smash);
-					}}
-				>
-					Smash
-				</li>
-				<li
-					className={
-						currentView.pathname === RoutePaths.catch ? 'active-view' : ''
-					}
-					onClick={() => {
-						navigate(RoutePaths.catch);
-					}}
-				>
-					Catch
-				</li>
-				<li
-					className={
-						currentView.pathname === RoutePaths.react ? 'active-view' : ''
-					}
-					onClick={() => {
-						navigate(RoutePaths.react);
-					}}
-				>
-					React
-				</li>
+					<img className={navHover ? 'spinning' : ''} src={logo} alt='' />
+					<li
+						className={
+							currentView.pathname === RoutePaths.home ? 'active-view' : ''
+						}
+						onClick={() => {
+							navigate(RoutePaths.home);
+						}}
+					>
+						Home
+					</li>
+					<li
+						className={
+							currentView.pathname === RoutePaths.smash ? 'active-view' : ''
+						}
+						onClick={() => {
+							navigate(RoutePaths.smash);
+						}}
+					>
+						Smash
+					</li>
+					<li
+						className={
+							currentView.pathname === RoutePaths.catch ? 'active-view' : ''
+						}
+						onClick={() => {
+							navigate(RoutePaths.catch);
+						}}
+					>
+						Catch
+					</li>
+					<li
+						className={
+							currentView.pathname === RoutePaths.react ? 'active-view' : ''
+						}
+						onClick={() => {
+							navigate(RoutePaths.react);
+						}}
+					>
+						React
+					</li>
+				</div>
+				<div className='right'>
+					<p>HÖGER SIDA</p>
+				</div>
 			</ul>
 		</div>
 	);
