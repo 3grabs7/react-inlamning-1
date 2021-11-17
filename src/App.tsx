c5bcdfd772ecfa75f4882fcb3493ac91d3e84783
Robin Grabs <robinmgrabs@gmail.com>
2021-11-17 23:16:31 +0100

    feature: add routing component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import { Nav } from './components/Nav';
+import { Routing } from './routes/Routing';
+
+enum appState {
+	GUEST = 'Guest',
+	PATRON = 'Patron',
+	VIP = 'Vip',
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Routing>
+			<Nav />
+		</Routing>
+	);
 }
 
 export default App;
