a4814bb5db49b2628cff51f0380ecd69dbcf4243
Robin Grabs <robinmgrabs@gmail.com>
2021-11-17 23:17:22 +0100

    feature: routing with type safing

---
+++ src/routes/Routing.tsx
@@ -0,0 +1,32 @@
+import PropTypes, { InferProps } from 'prop-types';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CatchView } from '../views/catch/CatchView';
+import { HomeView } from '../views/home/HomeView';
+import { NotFoundView } from '../views/not-found/NotFoundView';
+import { ReactView } from '../views/react/ReactView';
+import { SmashView } from '../views/smash/SmashView';
+import routePaths from './RoutePaths';
+
+export function Routing({ children }: InferProps<typeof Routing.propTypes>) {
+	return (
+		<BrowserRouter>
+			{children}
+			<Routes>
+				<Route path={routePaths.home} element={<HomeView />} />
+				<Route path={routePaths.smash} element={<SmashView />} />
+				<Route path={routePaths.catch} element={<CatchView />} />
+				<Route path={routePaths.react} element={<ReactView />} />
+				<Route path={''} element={<HomeView />} />
+				<Route path={'*'} element={<NotFoundView />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+Routing.propTypes = {
+	children: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.node),
+		PropTypes.node,
+	]),
+};
