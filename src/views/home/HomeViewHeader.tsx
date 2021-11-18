9d5b2a6af469050206d9766fa6de9305ba2e4d96
Robin Grabs <robinmgrabs@gmail.com>
2021-11-18 20:38:43 +0100

    feature: home view header component

---
+++ src/views/home/HomeViewHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const HomeViewHeader = () => {
+	return (
+		<div>
+			<h1>Welcome</h1>
+			<div>
+				<p>Play games, waste time, get pointless credit</p>
+				<p>It'll be fun. Kinda.</p>
+			</div>
+		</div>
+	);
+};
