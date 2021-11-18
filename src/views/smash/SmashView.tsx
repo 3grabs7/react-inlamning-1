a6334e886a77222bdc27f3e9ceb63d884c09bf28
Robin Grabs <robinmgrabs@gmail.com>
2021-11-18 20:39:43 +0100

    feature: add smash game component to smash view

--- src/views/smash/SmashView.tsx
+++ src/views/smash/SmashView.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { SmashGameContainer } from './SmashGameContainer';
 
 export const SmashView = () => {
-	return <div>smash view</div>;
+	return (
+		<div>
+			<SmashGameContainer />
+		</div>
+	);
 };
