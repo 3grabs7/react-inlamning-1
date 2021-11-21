2ff8505220e042787aece2c6285d024b707aa79b
Robin Grabs <robinmgrabs@gmail.com>
2021-11-21 01:22:50 +0100

    feature: add catch game container to view

--- src/views/catch/CatchView.tsx
+++ src/views/catch/CatchView.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { CatchGameContainer } from './CatchGameContainer';
 
 export const CatchView = () => {
+	return (
 		<div className='fade-in'>
+			<CatchGameContainer />
+		</div>
+	);
 };
