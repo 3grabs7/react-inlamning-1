3635731b8ce87818cf5886773f7095f7a3cf8a78
Robin Grabs <robinmgrabs@gmail.com>
2021-11-23 18:38:12 +0100

    feature: stats view with input to change name

--- src/views/stats/StatsView.tsx
+++ src/views/stats/StatsView.tsx
@@ -1,12 +1,36 @@
-import { useContext } from 'react';
-import { StatsContext } from '../../shared/stats-context';
+import { useState } from 'react';
+import { useStats, useStatsUpdate } from '../../shared/stats-context';
+import './StatsView.scss';
 
 export const StatsView = () => {
-	const stats = useContext(StatsContext);
+	const stats = useStats();
+	const statsChange = useStatsUpdate();
+	const [name, setName] = useState(stats?.name);
+
+	const handleNameChange = () => {
+		statsChange({
+			name,
+			currentLevel: stats?.currentLevel,
+			points: stats?.points,
+		});
+		setName('');
+	};
+
 	return (
 		<div>
+			<h1>Tjenna {stats?.name}</h1>
 			<h3>{stats?.currentLevel}</h3>
 			<p>{stats?.points}</p>
+
+			<div className='options'>
+				<h3>Change name</h3>
+				<input
+					type='text'
+					onChange={(e) => setName(e.target.value)}
+					value={name}
+				/>
+				<button onClick={handleNameChange}>Change</button>
+			</div>
 		</div>
 	);
 };
