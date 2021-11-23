f41cdde788947ef2d6dec28c9979b17dd22fce7b
Robin Grabs <robinmgrabs@gmail.com>
2021-11-23 14:26:20 +0100

    feature: stat context for stats view

--- src/views/stats/StatsView.tsx
+++ src/views/stats/StatsView.tsx
@@ -1,3 +1,12 @@
+import { useContext } from 'react';
+import { StatsContext } from '../../shared/stats-context';
+
 export const StatsView = () => {
-	return <div></div>;
+	const stats = useContext(StatsContext);
+	return (
+		<div>
+			<h3>{stats?.currentLevel}</h3>
+			<p>{stats?.points}</p>
+		</div>
+	);
 };
