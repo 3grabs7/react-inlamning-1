16219e6c584cd27ca949031019f09ad38779b737
Robin Grabs <robinmgrabs@gmail.com>
2021-11-23 18:37:46 +0100

    feature: home content

---
+++ src/views/home/HomeContent.tsx
@@ -0,0 +1,28 @@
+import { useContext } from 'react';
+import { useStats, useStatsUpdate } from '../../shared/stats-context';
+import './HomeContent.scss';
+
+export const HomeContent = () => {
+	const stats = useStats();
+	const statsUpdate = useStatsUpdate();
+
+	return (
+		<div className='home-content-wrapper'>
+			<div className='stats-wrapper'>
+				<div className='stats-box'>
+					<h3>Name</h3>
+					<h1>{stats?.name}</h1>
+				</div>
+				<div className='stats-box'>
+					<h3>Level</h3>
+					<h1>{stats?.currentLevel}</h1>
+				</div>
+				<div className='stats-box'>
+					<h3>Points</h3>
+					<h1>{stats?.points}</h1>
+				</div>
+				<div>FET IMAGE</div>
+			</div>
+		</div>
+	);
+};
