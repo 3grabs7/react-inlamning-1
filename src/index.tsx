0654764809b3106c498ec886ba2e73116483ca05
Robin Grabs <robinmgrabs@gmail.com>
2021-11-28 20:09:38 +0100

    feature: apollo client setup in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+	ApolloProvider,
+	ApolloClient,
+	createHttpLink,
+	InMemoryCache,
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+	uri: 'http://localhost:4000',
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
 );
 
 reportWebVitals();
