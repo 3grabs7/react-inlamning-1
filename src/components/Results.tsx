14ff40e5944d7e2b9781e072efc9843acd3dd631
Robin Grabs <robinmgrabs@gmail.com>
2021-12-06 10:05:59 +0100

    feature: mock data for loading results

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -2,31 +2,35 @@ import { Result } from './Result';
 import './Results.scss';
 
 export const Results = () => {
-	const games = [
-		{ name: 'hehe', scoreMetric: 'points' },
-		{ name: 'haha', scoreMetric: 'ms' },
-		{ name: 'hihi', scoreMetric: 'points' },
-		{ name: 'hoho', scoreMetric: 'ms' },
-	];
-	const results: { [key: string]: string[] } = {
-		hehe: ['123', '23', '789'],
-		haha: ['34', '234', '39824', '98234'],
-	};
+  const games = [
+    { name: 'hehe', scoreMetric: 'points' },
+    { name: 'haha', scoreMetric: 'ms' },
+    { name: 'hihi', scoreMetric: 'points' },
+    { name: 'hoho', scoreMetric: 'ms' },
+  ];
+  const results: { [key: string]: string[] } = {
+    hehe: ['123', '23', '789'],
+    haha: ['34', '234', '39824', '98234'],
+  };
 
-	return (
-		<div>
-			{games.map((game) =>
-				results[game.name] ? (
-					<div className='result-container'>
-						<h1>{game.name}</h1>
-						{results[game.name].map((result) => (
-							<Result score={result} scoreMetric={game.scoreMetric}></Result>
-						))}
-					</div>
-				) : (
-					<></>
-				)
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {games.map((game, gameIndex) =>
+        results[game.name] ? (
+          <div key={gameIndex} className='result-container'>
+            <h1>{game.name}</h1>
+            {results[game.name].map((result, resultIndex) => (
+              <Result
+                key={resultIndex}
+                score={result}
+                scoreMetric={game.scoreMetric}
+              ></Result>
+            ))}
+          </div>
+        ) : (
+          <></>
+        )
+      )}
+    </div>
+  );
 };
