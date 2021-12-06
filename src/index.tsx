330bc437597438c433269a60e5847d96517d7002
Robin Grabs <robinmgrabs@gmail.com>
2021-12-06 10:07:39 +0100

    refactor: remove apollo

--- src/index.tsx
+++ src/index.tsx
@@ -2,29 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-	ApolloProvider,
-	ApolloClient,
-	createHttpLink,
-	InMemoryCache,
-} from '@apollo/client';
-
-// url to our graphQL API
-const httpLink = createHttpLink({
-	uri: 'http://localhost:6969',
-});
-
-// init Apollo Client
-const client = new ApolloClient({
-	link: httpLink,
-	cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
-	<ApolloProvider client={client}>
-		<App />
-	</ApolloProvider>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
